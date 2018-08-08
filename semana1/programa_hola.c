0dcd2aac263d2ff0f229cc7a395ca7772f5e9b46
Cisneros Alvarez Maria Jose <cisnerosam2018@dci25-178.ugto.mx>
2018-08-08 10:13:30 -0500

    cree mi primer programa

---
+++ semana1/programa_hola.c
@@ -0,0 +1,14 @@
+/*
+ Esto es un comentario. Me sirve para escribir información relevante del programa que estoy haciendo. Por  ejemplo:
+ Este programa imprime la típica frase “hola mundo a la pantalla”
+ Creado el 06/08/2018 por Maria Jose Cisneros Alvarez
+*/
+
+/* creado el 8 de agosto del 2018 por Maria Jose Cisneros Alvarez*/
+#include<stdio.h>
+int main ()
+{
+	printf("hola Marijo\n");
+	printf("Este es el primer programa del curso\n");
+}	
+/*termine de escribir el programa*/
