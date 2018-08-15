5ff77a445b9d39cce9aa32ba62962513c69aa15b
Cisneros Alvarez Maria Jose <cisnerosam2018@dci25-187.ugto.mx>
2018-08-15 13:20:48 -0500

    "Este es un programa para realizar ciertas operaciones aritmeticas"

---
+++ semana2/programa_2.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include<math.h>
+
+
+
+int main()
+{
+	int a,b,c,d,e,f,g;
+		printf("Hola, dame un valor para a\n");
+	scanf("%i",&a);
+		printf("dame un valor para b\n");
+	scanf("%i",&b);
+		printf("dame un valor para c\n");
+	scanf("%i",&c);
+	printf("dame una variable para d\n");
+	scanf("%i",&d);
+	e=(a+b)*c/d;
+		f=((a+b)*c)/d;
+		g=a+(b*c)/d;
+		printf("\n tu primer resultado es:%i",e);
+	       printf("\n tu segundo resultado es:%i",f);
+       printf("\n tu tercer resultado es:%i",g);	
+}       
