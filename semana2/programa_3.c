c8b2d766b3c1951a455e3f72f753ca0b86b1a29f
Cisneros Alvarez Maria Jose <cisnerosam2018@dci25-187.ugto.mx>
2018-08-15 13:47:33 -0500

    Este programa nos permite ralizar ciertas opercaiones aritmenticas pero con muneros Reales.

---
+++ semana2/programa_3.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <math.h>
+
+int main ()
+{
+	float a,b,c,d, X1,X2,X3;
+		printf("asigna un valor a la variable a\n");
+		scanf("%f",&a);
+		printf("asigna un valor a la variable b\n");
+		scanf("%f",&b);
+		printf("asigna un valor a la variable c\n");
+		scanf("%f",&c);
+		printf("asigna un valor a la variable d\n");
+		scanf("%f",&d);
+			X1=(a+b)*c/d;
+X2=((a+b)*c)/d;
+			X3=a+(b*c)/d;
+			printf("%f primer resultado:\n",X1);
+			printf("%f segundo resultado:\n",X2);
+			printf("%f tercer resultado:\n",X3);
+			}
+
