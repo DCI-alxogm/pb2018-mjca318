b958aff97746ca930a606d60984a39e840c98280
Cisneros Alvarez Maria Jose <cisnerosam2018@dci25-186.ugto.mx>
2018-08-22 14:53:52 -0500

    trate de hacerlo

---
+++ semana3/funcionesxcoordenadas.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+#include<math.h>
+
+int main()
+{
+ int opc;
+	 int x, x1,x2,x3,X1,Y1,Z1,X2,Y2,Z2,X3,Y3,Z3;
+
+
+menu://coordenadas y funciones
+ printf("\n");
+ printf(" Opciones\n");
+ printf("Coordenada\n");
+ printf("Funcion\n");
+ printf("¿Cual eliges?\n");
+ scanf("%i",&opc);
+ switch(opc)
+ {
+case1:
+        printf("Asigna un valor a la variable x\n");
+        scanf("%f",&x);
+x1=exp(-X);
+x2=cos((X*3.1416)/180)+(2*tan((x*3.1416/180)));
+x3=log(X)+(3*pow(x,2));
+printf("x1 es igual a:%f\n",x1);
+        printf("x2 es igual a:%f\n",x2);
+        printf("\n x3 es igual a:%f",x3);
+ }
+ {
+case2:
+	 printf("Dame un valor para la coordenada X1\n");
+ }
