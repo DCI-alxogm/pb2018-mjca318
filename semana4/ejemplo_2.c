790bb39da7b9ef79639efb84689359ae51423992
Cisneros Alvarez Maria Jose <cisnerosam2018@dci25-163.ugto.mx>
2018-08-29 09:03:43 -0500

    hice los 2 ejemplos

---
+++ semana4/ejemplo_2.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+
+int main()
+{
+	float temp_C, temp_K;
+	float inicial=100, final=200, delta;
+	int n=10;
+	int op=1;
+
+	delta=(final-inicial)/n;
+	while(op==1){
+		temp_K=0.;
+		temp_C=inicial;
+
+		  while(temp_C<=final){
+                temp_K=temp_C+273.15;
+                printf("%f %f\n", temp_C, temp_K);
+                temp_C=temp_C+delta;           // temp_C+=delta;
+		  }
+		  printf("Deseas hacer otra operacion? presiona 1 para si, presiona 2 para no)\n");
+	scanf("%i",&op);
+	}
+	return 0;
+}
+
