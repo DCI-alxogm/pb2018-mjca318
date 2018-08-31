00431271fc8c045521f4b365759eda921da80246
Cisneros Alvarez Maria Jose <cisnerosam2018@dci25-163.ugto.mx>
2018-08-31 11:43:54 -0500

    "Read me"

---
+++ semana4/readme.d
@@ -0,0 +1,3 @@
+ Esta semana vimos el "condicional" while, que le indica al programa ejecutar acciones simultaneas. En nuestro caso juntamos elprograma de temperaturas y de coordenadas para dar la opción de regresar al menú inical y tener la posibilidad de realizar ambas acciomes (programas), o de realizar una y simplemente salir del programa.
+
+
