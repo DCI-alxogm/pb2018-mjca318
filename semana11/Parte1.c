24cf5cb9e5b77f4fef047a34cd8e8941ec638f92
mjca318 <42146408+mjca318@users.noreply.github.com>
2018-10-31 09:49:02 -0600

    Lo corregí porque me compilaba mal, por lo tanto no podia graficar. Apiadese de mi por favor, la quiero, dios la bendiga

---
+++ semana11/Parte1.c
@@ -0,0 +1,12 @@
+//Primer archivo de mi proyecto: aqui definire variables
+
+#include<stdio.h>
+#include<stdlib.h>
+
+void Proyecto2();
+int main(){ //Doy inicio al programa
+
+
+Proyecto2();
+return 0;
+}
